e5fcb7314849958cd5d0570e325868315df4f65a
Harleigh <155698437+Harweeeee@users.noreply.github.com>
2024-05-17 18:26:42 +1000

    Added footer information and styled

--- src/index.js
+++ src/index.js
@@ -7,5 +7,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
         <Dictionary />
+        <footer className="footer text-center">
+            Project coded by{' '}
+            <a href="https://github.com/Harweeeee">Harleigh Gallant</a>,
+            published on{' '}
+            <a href="https://github.com/Harweeeee/dictionary">Github</a> and
+            hosted on <a href="https://main--wordtome.netlify.app/y">Netlify</a>
+        </footer>
     </React.StrictMode>
 )
