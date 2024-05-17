232f55e9c92fcb96e85e63d48e410157bdbf631f
Harleigh <155698437+Harweeeee@users.noreply.github.com>
2024-05-17 14:01:40 +1000

    Imported axios, integrated API and ensure it's working

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,43 +1,69 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import './Dictionary.css'
+import Results from './Results'
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState(null)
+    let [results, setResults] = useState(null)
+
+    const apiKey = '263tdf1c5f75o8ab465cedabbe0f4604'
+    const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value)
     }
 
-    function search(event) {
-        event.preventDefault()
-        alert(`Searching for ${keyword}`)
+    function handleResponse(response) {
+        setResults(response.data)
     }
 
-    return (
-        <div className="Dictionary">
-            <div className="defineDictionary">
-                <h1>
-                    <strong>dictionary</strong>{' '}
-                </h1>
+    function search(event) {
+        event.preventDefault()
+        axios.get(apiUrl).then(handleResponse)
+    }
 
-                <p className="pNotation">/ˈdɪkʃən(ə)ri/</p>
+    if (!results) {
+        return (
+            <div className="Dictionary">
+                <div className="defineDictionary">
+                    <h1>
+                        <strong>dictionary</strong>{' '}
+                    </h1>
 
-                <p className="nounDictionary">
-                    a book or electronic resource that lists the words of a
-                    language (typically in alphabetical order) and gives their
-                    meaning, or gives the equivalent words in a different
-                    language, often also providing information about
-                    pronunciation, origin, and usage.
-                </p>
+                    <p className="pNotation">/ˈdɪkʃən(ə)ri/</p>
+
+                    <p className="nounDictionary">
+                        a book or electronic resource that lists the words of a
+                        language (typically in alphabetical order) and gives
+                        their meaning, or gives the equivalent words in a
+                        different language, often also providing information
+                        about pronunciation, origin, and usage.
+                    </p>
+                </div>
+                <form className="searchBox text-center" onSubmit={search}>
+                    <input
+                        type="search"
+                        className="inputWord"
+                        placeholder="Search the dictionary..."
+                        onChange={handleKeywordChange}
+                    ></input>
+                </form>
             </div>
-            <form className="searchBox text-center" onSubmit={search}>
-                <input
-                    type="search"
-                    className="inputWord"
-                    placeholder="Search the dictionary..."
-                    onChange={handleKeywordChange}
-                ></input>
-            </form>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="loadPage">
+                <form className="searchBox text-center" onSubmit={search}>
+                    <input
+                        type="search"
+                        className="inputWord"
+                        placeholder="Search the dictionary..."
+                        onChange={handleKeywordChange}
+                    ></input>
+                </form>
+                <Results results={results} />
+            </div>
+        )
+    }
 }
